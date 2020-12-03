688948b5262b2fd86653431200058bfec6f75a56
Vlad <holovlad@yandex.ru>
2020-12-03 22:53:28 +1000

    Правки

--- index.php
+++ index.php
@@ -2,6 +2,55 @@
 $is_auth = rand(0, 1);
 
 $user_name = 'Vlad'; // укажите здесь ваше имя
+
+$categories = [
+    'Доски и лыжи',
+    'Крепления',
+    'Ботинки',
+    'Одежда',
+    'Инструменты',
+    'Разное'
+
+];
+
+$lots = [
+    [
+        'title' => '2014 Rossignol District Snowboard',
+        'category' => 'Доски и лыжи',
+        'cost' => '10999',
+        'url' => 'img/lot-1.jpg'
+    ],
+    [
+        'title' => 'DC Ply Mens 2016/2017 Snowboard',
+        'category' => 'Доски и лыжи',
+        'cost' => '159999',
+        'url' => 'img/lot-2.jpg'
+    ],
+    [
+        'title' => 'Крепления Union Contact Pro 2015 года размер L/XL',
+        'category' => 'Крепления',
+        'cost' => '8000',
+        'url' => 'img/lot-3.jpg'
+    ],
+    [
+        'title' => 'Ботинки для сноуборда DC Mutiny Charocal',
+        'category' => 'Ботинки',
+        'cost' => '10999',
+        'url' => 'img/lot-4.jpg'
+    ],
+    [
+        'title' => 'Куртка для сноуборда DC Mutiny Charocal',
+        'category' => 'Одежда',
+        'cost' => '7500',
+        'url' => 'img/lot-5.jpg'
+    ],
+    [
+        'title' => 'Маска Oakley Canopy',
+        'category' => 'Разное',
+        'cost' => '5400',
+        'url' => 'img/lot-6.jpg'
+    ]
+];
 ?>
 <!DOCTYPE html>
 <html lang="ru">
@@ -60,17 +109,6 @@ $user_name = 'Vlad'; // укажите здесь ваше имя
                 <h2 class="promo__title">Нужен стафф для катки?</h2>
                 <p class="promo__text">На нашем интернет-аукционе ты найдёшь самое эксклюзивное сноубордическое и горнолыжное снаряжение.</p>
                 <ul class="promo__list">
-                    <?php
-                    $categories = [
-                        'Доски и лыжи',
-                        'Крепления',
-                        'Ботинки',
-                        'Одежда',
-                        'Инструменты',
-                        'Разное'
-
-                    ];
-                    ?>
                     <?php foreach ($categories as $val) : ?>
 
                         <li class="promo__item promo__item--boards">
@@ -84,46 +122,7 @@ $user_name = 'Vlad'; // укажите здесь ваше имя
                     <h2>Открытые лоты</h2>
                 </div>
                 <ul class="lots__list">
-                    <?php
-                    $lots = [
-                        [
-                            'title' => '2014 Rossignol District Snowboard',
-                            'category' => 'Доски и лыжи',
-                            'cost' => '10999',
-                            'url' => 'img/lot-1.jpg'
-                        ],
-                        [
-                            'title' => 'DC Ply Mens 2016/2017 Snowboard',
-                            'category' => 'Доски и лыжи',
-                            'cost' => '159999',
-                            'url' => 'img/lot-2.jpg'
-                        ],
-                        [
-                            'title' => 'Крепления Union Contact Pro 2015 года размер L/XL',
-                            'category' => 'Крепления',
-                            'cost' => '8000',
-                            'url' => 'img/lot-3.jpg'
-                        ],
-                        [
-                            'title' => 'Ботинки для сноуборда DC Mutiny Charocal',
-                            'category' => 'Ботинки',
-                            'cost' => '10999',
-                            'url' => 'img/lot-4.jpg'
-                        ],
-                        [
-                            'title' => 'Куртка для сноуборда DC Mutiny Charocal',
-                            'category' => 'Одежда',
-                            'cost' => '7500',
-                            'url' => 'img/lot-5.jpg'
-                        ],
-                        [
-                            'title' => 'Маска Oakley Canopy',
-                            'category' => 'Разное',
-                            'cost' => '5400',
-                            'url' => 'img/lot-6.jpg'
-                        ]
-                    ];
-                    ?>
+
                     <!--заполните этот список из массива с товарами-->
                     <?php foreach ($lots as $key => $val) : ?>
                         <li class="lots__item lot">
